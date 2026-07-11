0f2e8c709df597439356c66368c9b6620a542ba8
oj-builds <oloyeman52@gmail.com>
2026-07-11 16:58:16 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,42 +1,97 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    passwordHash: {
+      type: String,
+      default: null,
+    },
+
+    googleId: {
+      type: String,
+      default: null,
+    },
+
+    phone: {
+      type: String,
+      default: null,
+    },
+
+    avatar: {
+      type: String,
+      default: null,
+    },
+
+    bio: {
+      type: String,
+      default: "",
+    },
+
+    country: {
+      type: String,
+      default: "Nigeria",
+    },
+
+    referralCode: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+
+    referredBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+
+    referralCount: {
+      type: Number,
+      default: 0,
+    },
+
+    isPro: {
+      type: Boolean,
+      default: false,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+
+    resetTokenHash: {
+      type: String,
+      default: null,
+    },
+
+    resetTokenExpiry: {
+      type: Date,
+      default: null,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  passwordHash: {
-    type: String,
-    default: null,
-  },
-  googleId: {
-    type: String,
-    default: null,
-  },
-  phone: {
-    type: String,
-    default: null,
-  },
-  avatar: {
-    type: String,
-    default: null,
-  },
-  resetTokenHash: {
-    type: String,
-    default: null,
-  },
-  resetTokenExpiry: {
-    type: Date,
-    default: null,
-  },
-}, { timestamps: true });
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
